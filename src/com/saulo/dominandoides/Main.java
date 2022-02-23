d0a7738a9971c8d2e72d66f3c36a29b169ca48d4
sasasaulo <sasasaulo@gmail.com>
2022-02-23 19:38:45 -0300

    Criada a classe Cachorro

--- src/com/saulo/dominandoides/Main.java
+++ src/com/saulo/dominandoides/Main.java
@@ -1,15 +1,19 @@
 package com.saulo.dominandoides;
 
+
+import com.saulo.dominandoides.model.Cachorro;
 import com.saulo.dominandoides.model.Gato;
 
 public class Main {
 
-	public static void main(String[] args) {
-		
-		Gato gatinho1 = new Gato("Enzo","Amarelo",2);
-		
-		System.out.println(gatinho1);
+    public static void main(String[] args) {
 
-	}
+        Gato gatinho1 = new Gato("Enzo", "Amarelo", 2);
+        Cachorro cachorrinho = new Cachorro("Negresco", "Preto", 3);
+
+        System.out.println(cachorrinho);
+        System.out.println(gatinho1);
+
+    }
 
 }
