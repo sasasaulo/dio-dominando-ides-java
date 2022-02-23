4d84882753af8865d7ace54d8b763becd7007a29
sasasaulo <sasasaulo@gmail.com>
2022-02-23 16:45:30 -0300

    Primeiro commit usando STS

---
+++ src/com/saulo/dominandoides/Main.java
@@ -0,0 +1,15 @@
+package com.saulo.dominandoides;
+
+import com.saulo.dominandoides.model.Gato;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Gato gatinho1 = new Gato("Enzo","Amarelo",2);
+		
+		System.out.println(gatinho1);
+
+	}
+
+}
